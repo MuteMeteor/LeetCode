99334c873439b2b18ef6cedc99147ca172344f1b
kyrenewong <kyrenewong@163.com>
2017-10-15 21:57:14 +0800

    2017/10/15
    2017/10/15

---
+++ Python/HashTable/Two Sum.py
@@ -0,0 +1,25 @@
+# Given an array of integers, return indices of the two numbers
+# such that they add up to a specific target.
+#
+# You may assume that each input would have exactly one solution.
+#
+# Example:
+# Given nums = [2, 7, 11, 15], target = 9,
+#
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        table = {}
+        for i in range(len(nums)):
+            remain = target - nums[i]
+            if remain in table:
+                return table[remain], i
+            table[nums[i]] = i
+        return None
